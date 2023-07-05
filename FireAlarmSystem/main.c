496e665489db97ca693dbda33adb805cfe9b94f2
momen mohamed <momenmohamed64@gmail.com>
2023-07-05 13:12:44 +0300

    final main modification

--- FireAlarmSystem/main.c
+++ FireAlarmSystem/main.c
@@ -1,9 +1,34 @@
+#include "StdTypes.h"
+#include "DIO_Interface.h"
+#include "ADC_Interface.h"
+#include "LCD_Interface.h"
+#include "Sensors_interface.h"
+#include "KeyPad_Interface.h"
+#include "TEMP_Filter.h"
+#include "SMOKE_Filter.h"
+#include "SYSTEM_Manager.h"
+#define F_CPU 8000000
+#include <util/delay.h>
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	/*initialization*/
+	DIO_init();
+	ADC_Init(VREF_AVCC,ADC_SCALER_64);
+	LCD_Init();
+	KEYPAD_Init();
+	TEMP_FilterInit();
+	SMOKE_FilterInit();
+	SYSTEM_ManagerInit();
+	
+	while (1)
+	{
+		TEMP_FilterRunnable();
+		if (GetSystemStatus() != FINE)
+		{
+			SMOKE_FilterRunnable();
+		}
+		SYSTEM_ManagerRunnable();
+	}
 }
 
